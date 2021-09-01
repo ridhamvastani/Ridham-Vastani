44a73f968e5c076c40bd85685c8348e82677312f
ridhamvastani <89847611+ridhamvastani@users.noreply.github.com>
2021-09-01 11:09:20 +0530

    again upload day1 code assignment

--- day2.java
+++ day2.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
  
 public class GetStudentDetails
 {
-      public static void main(String args[])
+      public static void main(String[] args)
       {
           String name;
           int roll, math, phy, eng;
